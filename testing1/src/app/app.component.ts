084e456b313cb8d1c1baf5947d5a87bd2a4f4f88
lukas-soch <60264413+lukas-soch@users.noreply.github.com>
2021-01-29 13:31:48 -0500

    stuff

---
+++ testing1/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  ngOnInit() {
+  }
+  title = 'testing1';
+  @Input() timeV1;
+  @Input() timeV2;
+
+  timeChange1(val){
+    console.log(val)
+  }
+  timeChange2(val){
+    console.log(val)
+    // console.log(this.timeV2)
+  }
+}
